b240ef7dda38ae97d95d351bcaba1431c8db1450
Abel <zhenyucheung@163.com>
2011-11-07 23:51:53 +0800

    prime factors

--- src/main/java/zhang/abel/codekata/primefactors/PrimeFactors.java
+++ src/main/java/zhang/abel/codekata/primefactors/PrimeFactors.java
@@ -1,4 +1,19 @@
 package zhang.abel.codekata.primefactors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+
+    public static List<Integer> generate(int number) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        for (int candidate = 2; number > 1; candidate++) {
+            for (;number % candidate == 0; number /= candidate) {
+                result.add(candidate);
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/zhang/abel/codekata/primefactors/PrimeFactorsTest.java
+++ src/test/java/zhang/abel/codekata/primefactors/PrimeFactorsTest.java
@@ -2,6 +2,47 @@ package zhang.abel.codekata.primefactors;
 
 import org.junit.Test;
 
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static zhang.abel.codekata.primefactors.PrimeFactors.generate;
+
 public class PrimeFactorsTest {
 
+    @Test
+    public void should_return_emppty_list_given_input_1() {
+        assertThat(generate(1), is(Collections.EMPTY_LIST));
+    }
+
+    @Test
+    public void should_return_2_given_input_2() {
+        assertThat(generate(2), is(asList(2)));
+    }
+
+    @Test
+    public void should_return_3_given_input_3() {
+        assertThat(generate(3), is(asList(3)));
+    }
+
+    @Test
+    public void should_return_2_2_given_input_4() {
+        assertThat(generate(4), is(asList(2, 2)));
+    }
+
+    @Test
+    public void should_return_2_2_2_given_input_8() {
+        assertThat(generate(8), is(asList(2, 2, 2)));
+    }
+
+    @Test
+    public void should_return_3_3_given_input_9() {
+        assertThat(generate(9), is(asList(3, 3)));
+    }
+
+    @Test
+    public void should_() {
+        assertThat(generate(8765432), is(asList(1)));
+    }
 }
